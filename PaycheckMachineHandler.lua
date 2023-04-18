5105bf0d71e722a9a54e2ad361fa13638142bbc1
EmpyZ <98835977+EmpyZ@users.noreply.github.com>
2023-04-18 16:10:26 -0400

    Create PaycheckMachineHandler.lua

---
+++ PaycheckMachineHandler.lua
@@ -0,0 +1,41 @@
+local soundMod = require(game.ReplicatedStorage.Modules.SoundMod)
+
+local paycheckMachinesFolder = workspace.PaycheckMachines
+local paycheckMachines = paycheckMachinesFolder:GetChildren()
+
+local RequestPaycheck: RemoteFunction = game.ReplicatedStorage.Remotes.RequestPaycheck
+local UpdatePaycheckMachines: RemoteEvent = game.ReplicatedStorage.Remotes.UpdatePaycheckMachines
+
+local nextPaymentValue = 0
+
+local ost = os.time
+local debouncer = 0
+
+for _, paycheckMachine in paycheckMachines do
+	local pad: BasePart = paycheckMachine.PadComponents.Pad
+	
+	pad.Touched:Connect(function(hit)
+		local humanoid = hit.Parent:FindFirstChild("Humanoid")
+		if not humanoid or paycheckMachine:GetAttribute("Disabled") then return end
+		if ost() - debouncer < 1 then
+			return
+		end
+		
+		debouncer = ost()
+		local paycheck = RequestPaycheck:InvokeServer(nextPaymentValue)
+
+		if not paycheckMachine:GetAttribute("Disabled") then
+			soundMod:PlaySound("JanglingCoins","COINS!!!")
+		end	
+	end)
+end
+
+UpdatePaycheckMachines.OnClientEvent:Connect(function(amount)
+	nextPaymentValue = amount
+	for _, paycheckMachine in paycheckMachines do
+		local moneyLabel = paycheckMachine:FindFirstChild("MoneyLabel", true)
+		if moneyLabel then
+			moneyLabel.Text = tostring(amount)
+		end	
+	end
+end)

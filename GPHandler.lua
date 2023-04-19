371d8343ebc61e40a8bc379cc5cf20e3939c69d8
EmpyZ <98835977+EmpyZ@users.noreply.github.com>
2023-04-19 17:20:27 -0400

    Update GPHandler.lua

--- GPHandler.lua
+++ GPHandler.lua
@@ -1,16 +1,11 @@
-local UIActions = require(game:GetService("Players").LocalPlayer.PlayerScripts:WaitForChild("Folder").UIActions)
-
 local MarketplaceService = game:GetService("MarketplaceService")
-local Players = game:GetService("Players")
-local player = Players.LocalPlayer
 
-local PlayerGui = player:WaitForChild('PlayerGui')
-local ScreenGui = PlayerGui:WaitForChild("ScreenGui")
+local UIActions = require(script.Parent.UIActions)
 
-local gp = workspace.GP:GetDescendants()
+local Gamepasses = {}
 
 -- Function to prompt purchase of the Pass
-local function promptPurchase(passID, passName)
+function Gamepasses:PromptPurchase(player, ScreenGui, passID, passName)
 	local hasPass = false
 
 	local success, message = pcall(function()
@@ -23,17 +18,11 @@ local function promptPurchase(passID, passName)
 	end
 
 	if hasPass then
-		UIActions:CloneText(("You already own the %s Gamepass"):format(passName),ScreenGui)
+		UIActions:CloneText(("You already own the %s Gamepass"):format(passName) , ScreenGui, 1.5)
 	else
 		-- Player does NOT own the Pass; prompt them to purchase
 		MarketplaceService:PromptGamePassPurchase(player, passID)
 	end
 end
 
-for _, prompt in gp do
-	if prompt:IsA("ProximityPrompt") then
-		prompt.Triggered:Connect(function(plr)
-			promptPurchase(prompt:FindFirstAncestor("Gamepass"):FindFirstChild("GameProduct").Value ,prompt:FindFirstAncestor("Gamepass").Parent.Name)
-		end)
-	end
-end
+return Gamepasses

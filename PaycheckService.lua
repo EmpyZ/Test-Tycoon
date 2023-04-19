c08b738bbd4485a0985b6c81da61e2fb2d31875f
EmpyZ <98835977+EmpyZ@users.noreply.github.com>
2023-04-19 17:22:25 -0400

    Update PaycheckService.lua

--- PaycheckService.lua
+++ PaycheckService.lua
@@ -29,16 +29,22 @@ end
 Players.PlayerAdded:Connect(onPlayerAdded)
 
 RequestPaycheck.OnServerInvoke = function(player, amount)
-	if ost() - PlayerData:CheckDebounce(player) < 1 then return end
+	if ost() - PlayerData:CheckDebounce(player) < 1 or PlayerData:GetPaycheckWithdrawAmount(player) == 0 then return 0,false end
 	if PlayerData:GetGamepassInfo(player,"Auto Collect") then
 		workspace.PaycheckMachines.PaycheckMachine:SetAttribute("Disabled", true)
-		return
+		return 0, false
 	end
-	PlayerData:AddMoney(player, amount)
-
+	
+	if amount > PlayerData:GetPaycheckWithdrawAmount(player) then warn("looks like someone is tampering with the local scripts, possibly ban or kick them")
+		PlayerData:SetPaycheckWithdrawAmount(player, -100000000000)
+		return 0, false
+	end
+	local debounce = PlayerData:SetDebounce(player, ost())
+	PlayerData:AddMoney(player, PlayerData:GetPaycheckWithdrawAmount(player))
+	--print(PlayerData:GetPaycheckWithdrawAmount(player))
+	--print(PlayerData:GetMoney(player))
 	local paycheck = PlayerData:SetPaycheckWithdrawAmount(player, 0)
 	UpdatePaycheckMachines:FireClient(player, PlayerData:GetPaycheckWithdrawAmount(player))
 	
-	local debounce = PlayerData:SetDebounce(player, ost())
-	return amount
+	return amount, true
 end
